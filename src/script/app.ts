b8eeab50dd8972b66fedbcfca89c460ad733b775
WMXPY <wm@mengw.io>
2018-07-19 23:59:24 -0500

    bump version number

--- src/script/app.ts
+++ src/script/app.ts
@@ -30,7 +30,7 @@ const uploadSingle = Upload().single('image');
 app.all('*', (req: Request, res: Response, next: NextFunction) => {
     res.header("Access-Control-Allow-Origin", Config.crossOrigin);
     res.header("X-Powered-By", 'Markus');
-    res.header("X-Markus-Version", "1.1.1");
+    res.header("X-Markus-Version", "1.2.0");
     next();
 });
 
