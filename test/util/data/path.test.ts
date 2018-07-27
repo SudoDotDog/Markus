a93efa2cb2ec5abd131870663c8d97090841d370
WMXPY <wm@mengw.io>
2018-07-26 19:40:28 -0500

    try to fix test

--- test/util/data/path.test.ts
+++ test/util/data/path.test.ts
@@ -4,16 +4,19 @@
  */
 
 import { expect } from 'chai';
+import * as Path from 'path';
 import { fileBuilder, pathBuilder } from '../../../src/util/data/path';
 
 describe('test path builder functions', (): void => {
     it('path builder should return builded path', (): void => {
         const result: string = pathBuilder('folder', 'test');
-        expect(result).to.be.equal('test\\folder');
+        const expected: string = Path.join('test', 'folder');
+        expect(result).to.be.equal(expected);
     });
 
     it('file builder should return builded file path', (): void => {
         const result: string = fileBuilder('folder', 'file', 'test');
-        expect(result).to.be.equal('test\\folder\\file');
+        const expected: string = Path.join('test', 'folder', 'file');
+        expect(result).to.be.equal(expected);
     });
 });
