a25dbf60124dfb4c3ac993c08e69da68ac07397c
WMXPY <wm@mengw.io>
2018-08-13 20:45:11 -0500

    fix import of mock tool

--- test/mock/tool.ts
+++ test/mock/tool.ts
@@ -4,17 +4,23 @@
  * @fileoverview Mock Markus Tool
  */
 
-import { IMarkusResult, IMarkusTool, MarkusController, MarkusDirect, MarkusRequireType } from "../../src/toolbox/toolbox";
+import {
+    IMarkusResult,
+    IMarkusTool,
+    MARKUS_REQUIRE_TYPE,
+    MarkusController,
+    MarkusDirect,
+} from "../../src/toolbox/toolbox";
 
 export class MockMarkusTool implements IMarkusTool {
     public name: string;
     public description: string;
-    public require: MarkusRequireType[];
+    public require: MARKUS_REQUIRE_TYPE[];
 
     private _controller: MarkusController;
     private _direct: MarkusDirect;
 
-    public constructor(name: string, description: string, require: MarkusRequireType[]) {
+    public constructor(name: string, description: string, require: MARKUS_REQUIRE_TYPE[]) {
         this.name = name;
         this.description = description;
         this.require = require;
