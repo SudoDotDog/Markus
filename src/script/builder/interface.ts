efb80059785ab74c935ff83368a2d1decbbd862b
C <pc@wmpcxpy.com>
2018-08-23 15:24:29 -0500

    Update interface.ts

--- src/script/builder/interface.ts
+++ src/script/builder/interface.ts
@@ -4,8 +4,18 @@ export interface IExpressBuilder {
     flush: () => any;
 }
 
+export enum ROUTE_MODE {
+    POST = 'POST',
+    GET = 'GET',
+    DELETE = 'DELETE',
+    PUT = 'PUT',
+    ALL = 'ALL',
+}
+
 export interface IExpressRoute {
     readonly path: string;
+    readonly route: ROUTE_MODE;
     readonly stack: any[];
     readonly mode: any;
+    readonly authorization: boolean;
 }
