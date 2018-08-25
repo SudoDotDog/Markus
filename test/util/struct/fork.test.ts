4c26f4f795fe9804dbae7361a63d36b56db11b0f
WMXPY <wm@mengw.io>
2018-08-25 12:54:36 -0500

    upgreade fork's test

--- test/util/struct/fork.test.ts
+++ test/util/struct/fork.test.ts
@@ -17,4 +17,34 @@ describe('test queue data structure', (): void => {
         const fork: Fork<number> = new Fork();
         expect(fork).to.be.lengthOf(0);
     });
+
+    it('add element will push it to list', (): void => {
+        const fork: Fork<number> = new Fork();
+        fork.add(1);
+        fork.add(1);
+        fork.add(3);
+        expect(fork).to.be.lengthOf(3);
+    });
+
+    it('has element return if has anything in it', (): void => {
+        const fork: Fork<{
+            name: string;
+            value: number;
+        }> = new Fork();
+        fork.add({
+            name: 'test',
+            value: 1,
+        });
+        fork.add({
+            name: 'test-2',
+            value: 1,
+        });
+
+        const result: boolean = fork.has((element) => element.name === 'test');
+        // tslint:disable-next-line
+        expect(result).to.be.true;
+        const result2: boolean = fork.has((element) => element.name === 'test-3');
+        // tslint:disable-next-line
+        expect(result).to.be.false;
+    });
 });
