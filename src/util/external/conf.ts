660ec1ea0ac80358f86b37d82ab1e1b466e02b2e
PCXPY <pc@wmpcxpy.com>
2018-08-28 11:03:45 -0500

    [Clean] Move hard code to template file

--- src/util/external/conf.ts
+++ src/util/external/conf.ts
@@ -8,26 +8,9 @@ import bkc from 'bkc';
 import { IBkcOptions, ICallable } from 'bkc/dist/types/callable';
 import * as Fs from 'fs';
 import * as Path from 'path';
-import { IMarkusConfConfig, MODE } from '../../interface';
+import { IMarkusConfConfig } from '../../interface';
 import { error, ERROR_CODE } from '../error/error';
-
-export const getMarkusConfigTemplate = (): IMarkusConfConfig => {
-    return {
-        host: 'mongodb://localhost:27017',
-        database: 'markus-test-2',
-        imagePath: 'F://path/image',
-        tempPath: 'F://path/temp',
-        imagePFolder: 5,
-        isDebug: true,
-        maxThread: 4,
-        uploadLimit: 25,
-        portNumber: 8080,
-        verbose: false,
-        white404ImagePath: Path.resolve('assets/404image_white.png'),
-        black404ImagePath: Path.resolve('assets/404image_black.png'),
-        mode: MODE.FILE_SYSTEM,
-    };
-};
+import { getMarkusConfigTemplate } from './template';
 
 export default class MarkusConfigReader {
     private _config: IMarkusConfConfig;
---
+++ src/util/external/template.ts
@@ -0,0 +1,30 @@
+/**
+ * @author PCXPY
+ * @deprecated External Utils
+ * @fileoverview Config Templates
+ */
+
+import * as Path from 'path';
+import { IMarkusConfConfig, MODE } from "../../interface";
+
+export const getMarkusConfigTemplate = (): IMarkusConfConfig => {
+    return {
+        host: 'mongodb://localhost:27017',
+        database: 'markus-test-2',
+        imagePath: 'F://path/image',
+        tempPath: 'F://path/temp',
+        imagePFolder: 5,
+        isDebug: true,
+        maxThread: 4,
+        uploadLimit: 25,
+        portNumber: 8080,
+        verbose: false,
+        white404ImagePath: Path.resolve('assets/404image_white.png'),
+        black404ImagePath: Path.resolve('assets/404image_black.png'),
+        mode: MODE.FILE_SYSTEM,
+    };
+};
+
+export const verifyMarkusConfigKey = (key: string): boolean => {
+    return true;
+};
