ce5265dd0a7c6f3982c27405422d8ff21a697400
WMXPY <wm@mengw.io>
2018-09-01 22:56:30 -0500

    fix import format

--- src/service/extension/import.ts
+++ src/service/extension/import.ts
@@ -4,8 +4,6 @@
  * @fileoverview Imports
  */
 
-import ExtensionBodyParserJson from './body_parser_json';
-import ExtensionDocGenerate from './doc_generate';
-
-export { ExtensionBodyParserJson, ExtensionDocGenerate };
+export { default as ExtensionBodyParserJson } from './body_parser_json';
+export { default as ExtensionDocGenerate } from './doc_generate';
 
