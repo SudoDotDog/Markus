0117aff600e4daaa728225c7fffbdd68d0d93b12
WMXPY <wm@mengw.io>
2018-09-12 15:26:29 -0500

    fix icon test

--- test/icon/sparidae/buffer.test.ts
+++ test/icon/sparidae/buffer.test.ts
@@ -24,21 +24,21 @@ describe('test buffer parser', () => {
         testContent.rect(textEndPoint, textEndPoint, textEndPoint, '10');
 
         const result = testContent.flush();
-        expect(result).to.be.equal('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 480" version="1.1" preserveAspectRatio="none" ><polygon points="15,12 15,12 15,12" fill="10" /></svg>');
+        expect(result).to.be.equal('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 480" version="1.1" preserveAspectRatio="none"><g><polygon points="15,12 15,12 15,12" fill="10" /></g></svg>');
     });
 
     it('rect will put polygon with aspect to target position', () => {
         testContent.rect(textEndPoint, textEndPoint, textEndPoint, '10');
 
-        testContent.setAspect(true); // TODO
+        testContent.setAspect(true);
         const result = testContent.flush();
-        expect(result).to.be.equal('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 480" version="1.1" preserveAspectRatio="none" ><polygon points="15,12 15,12 15,12" fill="10" /></svg>');
+        expect(result).to.be.equal('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 480" version="1.1" preserveAspectRatio="true"><g><polygon points="15,12 15,12 15,12" fill="10" /></g></svg>');
     });
 
     it('text will put text to target position', () => {
         testContent.text(textEndPoint, 10);
 
         const result = testContent.flush();
-        expect(result).to.be.equal('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 480" version="1.1" preserveAspectRatio="none" ><text x="15" y="12" style="font-weight:bold;font-size:10;text-anchor:end">testText</text></svg>');
+        expect(result).to.be.equal('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 480 480" version="1.1" preserveAspectRatio="none"><g><text x="15" y="12" style="font-weight:bold;font-size:10;text-anchor:end">testText</text></g></svg>');
     });
 });
