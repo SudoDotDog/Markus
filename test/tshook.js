409edf23eeabcbd860141d53b4f941c64db8c45b
WMXPY <wm@mengw.io>
2018-09-14 15:18:23 -0500

    TYPES!!

--- test/tshook.js
+++ test/tshook.js
@@ -1,21 +1,4 @@
-const tsnode = require("ts-node")
+const tsnode = require("ts-node");
 tsnode.register({
-    compilerOptions: {
-        sourceMap: true,
-        jsx: "react",
-        strict: true,
-        noImplicitReturns: true,
-        noImplicitAny: true,
-        module: "commonjs",
-        experimentalDecorators: true,
-        emitDecoratorMetadata: true,
-        moduleResolution: "node",
-        target: "es6",
-        lib: [
-            "dom",
-            "es2016",
-            "es2017",
-            "es6",
-        ]
-    },
+    project: 'typescript/tsconfig.test.json',
 });
