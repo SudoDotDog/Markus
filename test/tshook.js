b9c9d7f217a704b428e19ec9a967f78eee9dc9c1
WMXPY <wm@mengw.io>
2018-09-14 23:52:18 -0500

    fix for node 7

--- test/tshook.js
+++ test/tshook.js
@@ -1,9 +1,8 @@
 const tsnode = require("ts-node");
 
-process.env = {
-    ...process.env,
+process.env = Object.assign(process.env, {
     NODE_ENV: 'test',
-};
+});
 
 tsnode.register({
     project: 'typescript/tsconfig.test.json',
