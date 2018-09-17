e08f76a9e0cda822010aaebeb624cd287adf95ec
WMXPY <wm@mengw.io>
2018-09-17 13:49:25 -0500

    init test drive react component

--- src/doc/components/testDrive.tsx
+++ src/doc/components/testDrive.tsx
@@ -20,23 +20,35 @@ export interface IProps {
 export default class TestDrive extends React.Component<IProps, {}> {
     public constructor(props: IProps) {
         super(props);
-
-        this.inputBoxes = this.inputBoxes.bind(this);
     }
 
     public render(): JSX.Element {
         return (<div>
             {this.inputBoxes()}
+            <button>Send</button>
         </div>);
     }
 
     protected inputBoxes() {
+        const route: IExpressRoute = this.props.route;
         const elements: any[] = [];
-        const body: IExpressAssertionJSONType = this.props.route.assertBody || {};
-        for(let key of Object.keys(body)){
+
+        const body: IExpressAssertionJSONType = route.assertBody || {};
+        for (let key of Object.keys(body)) {
             elements.push(body[key].type.toString());
         }
 
+        if (route.authorization) {
+            elements.push(this.input('AuthKey', 'auth'))
+        }
+
         return elements;
+    }
+
+    protected input(name: string, key: string): JSX.Element {
+        return (<div key={key}>
+            {name}
+            <input data-test-drive-key={key}/>
+        </div>);
     }
 }
