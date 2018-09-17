95afd2a6dbe30aebad549907bab71bc926778914
WMXPY <wm@mengw.io>
2018-09-17 10:51:33 -0500

    init test drive component

---
+++ src/doc/components/testDrive.tsx
@@ -0,0 +1,22 @@
+/**
+ * @author WMXPY
+ * @description Doc React
+ * @fileoverview TestDrive
+ */
+
+import * as React from "react";
+
+const styles: {
+    [key: string]: React.CSSProperties;
+} = {
+};
+
+export interface IProps {
+}
+
+export default class TestDrive extends React.Component<IProps, {}> {
+    public render(): JSX.Element {
+        return (<div>
+        </div>);
+    }
+}
